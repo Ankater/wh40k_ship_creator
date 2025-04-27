b62a3aab7aac36a5028d8f14f48e0cd24f6c3d49
ankatern <ankatern@gmail.com>
2025-04-27 21:33:43 +0500

    fix phpstan

--- app/Http/Resources/ShipComplicationResource.php
+++ app/Http/Resources/ShipComplicationResource.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Resources;
 
+use App\Models\ShipComplications;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin ShipComplications
+ */
 class ShipComplicationResource extends JsonResource
 {
     /**
